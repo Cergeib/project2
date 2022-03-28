7120b660bd055c8f0048a393ab58274404998b20
t-ianshina <t.ianshina99@gmail.com>
2022-03-29 02:53:47 +0300

    start2

--- test.py
+++ test.py
@@ -1,3 +1,18 @@
-print('hi')
-a = 724
-print(a)
+from Bio.SeqIO import parse
+from Bio.SeqRecord import SeqRecord
+from Bio.Seq import Seq
+
+vector = open("C:/Users/yeba/Desktop/vector.fasta")
+
+records = parse(vector, "fasta")
+for record in records:
+#   print("Id: %s" % record.id)
+#   print("Name: %s" % record.name)
+#   print("Description: %s" % record.description)
+#   print("Annotations: %s" % record.annotations)
+#   print("Sequence Data: %s" % record.seq)
+    vector_sequence = record.seq
+#   print("Sequence Alphabet: %s" % record.seq.alphabet)
+
+print(vector_sequence)
+
